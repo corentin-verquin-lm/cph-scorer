b24f0ba325338e49565537cf54577ed14a208d66
corentin-verquin-lm <corentin.verquin@ext.adeo.com>
2025-04-20 18:22:51 +0200

    init

---
+++ infra/variables.tf
@@ -0,0 +1,12 @@
+data "aws_caller_identity" "current" {}
+data "aws_ssm_parameter" "db_user" {
+  name = "/cph/prod/db_user"
+
+}
+data "aws_ssm_parameter" "db_password" {
+  name = "/cph/prod/db_password"
+  with_decryption = true
+}
+data "aws_ssm_parameter" "db_name" {
+  name = "/cph/prod/db_name"
+}
